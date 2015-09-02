dd5dfbad0b2cf1e2a825fb1641fb89a4cbb77ed7
powerboat9 <powerboat9@users.noreply.github.com>
2015-09-02 15:37:51 -0400

    Update core.lua

--- Tick_Tack_Toe/core.lua
+++ Tick_Tack_Toe/core.lua
@@ -1,1 +1,37 @@
-function getAllCombinations(
+function doIndexMatch(t1, t2)
+    if #t1 ~= #t2 then
+        return false
+    end
+    for k, v in ipairs(t1) do
+        if t2[k] ~= v then
+            return false
+        end
+    end
+    return true
+end
+
+function genColumn()
+    local column = {}
+    for i = 1, 3 do
+        math.randomseed(os.time())
+        column[i] = math.random() >= 0.5
+    end
+    return column
+end
+
+function getCombinations()
+    local combinations = {}
+    for i = 1, 511 do
+        local editI = i
+        for j = 9, 1, -1 do
+            local magic = math.pow(2, i - 1)
+            if editI >= magic then
+                combinations[i][j] = true
+                editI = editI - magic
+            else
+                combinations[i][j] = false
+            end
+        end
+    end
+    return combinations
+end
